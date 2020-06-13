e01e2e62cf72a2ce966f629946df95d270bb8bd6
SoaringGecko <48862796+SoaringGecko@users.noreply.github.com>
2020-06-13 15:16:42 -0400

    Add files via upload

---
+++ Buffs/TheMoneyTroughPetBuff.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MoneyTroughPet.Buffs
+{
+	public class TheMoneyTroughPetBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			// DisplayName and Description are automatically set from the .lang files, but below is how it is done normally.
+			// DisplayName.SetDefault("Paper Airplane");
+			// Description.SetDefault("\"Let this pet be an example to you!\"");
+			Main.buffNoTimeDisplay[Type] = true;
+			Main.vanityPet[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.buffTime[buffIndex] = 18000;
+			bool petProjectileNotSpawned = player.ownedProjectileCounts[(ProjectileID.FlyingPiggyBank)] <= 0;
+			if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
+			{
+				Projectile.NewProjectile(player.position.X + player.width / 2, player.position.Y + player.height / 2, 0f, 0f, (ProjectileID.FlyingPiggyBank), 0, 0f, player.whoAmI, 0f, 0f);
+			}
+		}
+	}
+}
---
+++ Items/MoneyTroughPet.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MoneyTroughPet.Items
+{
+	public class MoneyTroughPet : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName and Tooltip are automatically set from the .lang files, but below is how it is done normally.
+			DisplayName.SetDefault("Money Trough Pet");
+			Tooltip.SetDefault("Summons the Money Trough as a pet");
+		}
+
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.MoneyTrough);
+			item.shoot = (ProjectileID.FlyingPiggyBank);
+			item.buffType = BuffType<Buffs.TheMoneyTroughPetBuff>();
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.MoneyTrough);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void UseStyle(Player player)
+		{
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+			{
+				player.AddBuff(item.buffType, 3600, true);
+			}
+		}
+	}
+}
